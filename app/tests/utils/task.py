4308e99ebeec4991f213d6abf5539dd385b00b4c
Dimosthenis Schizas <dimoschi@gmail.com>
2022-03-23 04:18:21 +0200

    Add create_random_line in test utils

--- app/tests/utils/task.py
+++ app/tests/utils/task.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 import random
 
 from app import crud, models
+from app.schemas.lines.line import LineCreate
 from app.schemas.task import TaskCreate
 from app.schemas.task_result import TaskResultCreate
 from app.tests.utils.utils import random_from_lines_list, random_lower_string
@@ -23,6 +24,21 @@ def create_random_task(db: Session) -> models.Task:
     return crud.task.create(db=db, obj_in=task_in)
 
 
-def create_random_task_results(db: Session, task: models.Task) -> models.Task:
-    task_in = TaskResultCreate(task_id=task.id, line_id=random.randint(1, 10))
+def create_random_line(db: Session) -> models.Line:
+    line = random_from_lines_list()[0]
+    line_in = LineCreate(
+        tfl_id=line,
+        tfl_name=line,
+        mode_id="tube",
+        created=datetime.now(),
+        modified=datetime.now(),
+    )
+
+    return crud.line.create(db=db, obj_in=line_in)
+
+
+def create_random_task_results(
+    db: Session, task: models.Task, line: models.Line
+) -> models.TaskResult:
+    task_in = TaskResultCreate(task_id=task.id, line_id=line.id)
     return crud.task_result.create(db=db, obj_in=task_in)
