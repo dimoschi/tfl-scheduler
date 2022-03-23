217838b86934f5cd16a9fbcf80ced4b48d870384
Dimosthenis Schizas <dimoschi@gmail.com>
2022-03-23 04:01:11 +0200

    Add method to create TestResult

--- app/tests/utils/task.py
+++ app/tests/utils/task.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 
 from sqlalchemy.orm import Session
+import random
 
 from app import crud, models
 from app.schemas.task import TaskCreate
+from app.schemas.task_result import TaskResultCreate
 from app.tests.utils.utils import random_from_lines_list, random_lower_string
 
 
@@ -19,3 +21,8 @@ def create_random_task(db: Session) -> models.Task:
         lines=lines,
     )
     return crud.task.create(db=db, obj_in=task_in)
+
+
+def create_random_task_results(db: Session, task: models.Task) -> models.Task:
+    task_in = TaskResultCreate(task_id=task.id, line_id=random.randint(1, 10))
+    return crud.task_result.create(db=db, obj_in=task_in)
