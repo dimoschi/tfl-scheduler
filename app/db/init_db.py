76c6d72241c4a7f2d79be1bfa271c3bca5727429
Dimosthenis Schizas <dimoschi@gmail.com>
2022-03-23 04:18:01 +0200

    Remove unused code from init_db

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,7 +1,5 @@
 from sqlalchemy.orm import Session
 
-from app import crud, schemas
-from app.core.config import settings
 from app.db import base  # noqa: F401
 
 # make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
@@ -14,12 +12,4 @@ def init_db(db: Session) -> None:
     # But if you don't want to use migrations, create
     # the tables un-commenting the next line
     # Base.metadata.create_all(bind=engine)
-
-    user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
-    if not user:
-        user_in = schemas.UserCreate(
-            email=settings.FIRST_SUPERUSER,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            is_superuser=True,
-        )
-        user = crud.user.create(db, obj_in=user_in)  # noqa: F841
+    pass
