3e30d9ce44b8c8a19f5aa01d869574e4813e0f64
Dimosthenis Schizas <dimoschi@gmail.com>
2022-03-23 04:18:37 +0200

    Add/Fix tests

--- app/tests/api/api_v1/test_tasks.py
+++ app/tests/api/api_v1/test_tasks.py
@@ -2,7 +2,11 @@ from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
 from app.core.config import settings
-from app.tests.utils.task import create_random_task
+from app.tests.utils.task import (
+    create_random_task,
+    create_random_task_results,
+    create_random_line,
+)
 
 
 def test_create_task(client: TestClient, db: Session) -> None:
@@ -20,7 +24,7 @@ def test_create_task(client: TestClient, db: Session) -> None:
     content = response.json()
     assert content["title"] == data["title"]
     assert content["description"] == data["description"]
-    assert content["job"] is not None
+    assert content["job_id"] is not None
     assert "id" in content
 
 
@@ -38,7 +42,7 @@ def test_create_task_without_schedule_time(client: TestClient, db: Session) -> N
     content = response.json()
     assert content["title"] == data["title"]
     assert content["description"] == data["description"]
-    assert content["job"] is None
+    assert content["job_id"] is None
     assert "id" in content
 
 
@@ -68,11 +72,82 @@ def test_read_task_not_found(client: TestClient, db: Session) -> None:
 def test_update_task(client: TestClient, db: Session) -> None:
     task = create_random_task(db)
     new_title = "new_" + task.title
-    response = client.put(
-        f"{settings.API_V1_STR}/tasks/{task.id}", data={"title": new_title}
-    )
-    assert response.status_code == 422
+    data = {"title": new_title}
+    response = client.patch(f"{settings.API_V1_STR}/tasks/{task.id}", json=data)
+    assert response.status_code == 200
     content = response.json()
     assert content["title"] != task.title
     assert content["description"] == task.description
     assert content["id"] == task.id
+
+
+def test_update_task_not_found(client: TestClient, db: Session) -> None:
+    task = create_random_task(db)
+    new_title = "new_" + task.title
+    data = {"title": new_title}
+    task_id = task.id + 1000
+    response = client.patch(f"{settings.API_V1_STR}/tasks/{task_id}", json=data)
+    assert response.status_code == 404
+    content = response.json()
+    assert content["detail"] == "Task not found"
+
+
+def test_get_tasks(client: TestClient, db: Session) -> None:
+    task1 = create_random_task(db)
+    task2 = create_random_task(db)
+    response = client.get(
+        f"{settings.API_V1_STR}/tasks",
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert content[0]["title"] != task1.title
+    assert content[1]["title"] != task2.title
+    assert content[0]["id"] != task1.id
+    assert content[1]["id"] != task2.id
+
+
+def test_read_task_results(client: TestClient, db: Session) -> None:
+    task = create_random_task(db)
+    line = create_random_line(db)
+    task_result = create_random_task_results(db, task=task, line=line)
+    response = client.get(
+        f"{settings.API_V1_STR}/tasks/results/{task.id}",
+    )
+    assert response.status_code == 200
+    content = response.json()
+    assert isinstance(content, list)
+    assert len(content) > 0
+    assert content[0]["id"] == task_result.id
+    assert content[0]["task_id"] == task.id
+
+
+def test_read_task_results_task_not_found(client: TestClient, db: Session) -> None:
+    task = create_random_task(db)
+    line = create_random_line(db)
+    create_random_task_results(db, task=task, line=line)
+    task_id = task.id + 1000
+    response = client.get(
+        f"{settings.API_V1_STR}/tasks/results/{task_id}",
+    )
+    assert response.status_code == 404
+    content = response.json()
+    assert content["detail"] == "Task not found"
+
+
+def test_delete_task(client: TestClient, db: Session) -> None:
+    task = create_random_task(db)
+    response = client.delete(f"{settings.API_V1_STR}/tasks/{task.id}")
+    assert response.status_code == 200
+    content = response.json()
+    assert content["title"] == task.title
+    assert content["description"] == task.description
+    assert content["id"] == task.id
+
+
+def test_delete_task_not_found(client: TestClient, db: Session) -> None:
+    task = create_random_task(db)
+    task_id = task.id + 1000
+    response = client.delete(f"{settings.API_V1_STR}/tasks/{task_id}")
+    assert response.status_code == 404
+    content = response.json()
+    assert content["detail"] == "Task not found"
